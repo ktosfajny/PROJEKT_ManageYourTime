1aef536df9a3a5fe1a235af0a33f95d6b641328c
kto <ktosfajny777@interia.pl>
2020-06-15 10:52:44 +0200

    Tasks Collection

---
+++ TasksCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ManageYourTime
+{
+    public class TasksCollection
+    {
+
+        public TasksCollection()
+        {
+            InitListWithTasks();
+        }
+
+
+        public ObservableCollection<Task> ListwithTasks { get; set; }
+
+        public Task WybranyTask { get; set; }
+
+        public void InitListWithTasks()
+        {
+            ListwithTasks = new ObservableCollection<Task>();
+            ListwithTasks.Add(new Task(1, "zrobić zakupy", "Spotkanie", "28-07-2020"));
+        }
+
+
+    }
+}
