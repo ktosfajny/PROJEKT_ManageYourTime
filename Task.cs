381a07b54ada7bd540f9b477b7e00bfd2510025c
kto <ktosfajny777@interia.pl>
2020-06-15 10:49:01 +0200

    Task class

---
+++ Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManageYourTime
+{
+    public class Task
+    {
+        public int Nr { get; set; }
+
+        public string Nazwa { get; set; }
+
+        public string Rodzaj { get; set; }
+
+        public string Data { get; set; }
+
+
+        public Task()
+        {
+            Nr = 1;
+        }
+
+        public Task(int nr, string nazwa, string rodzaj, string data)
+        {
+            this.Nr = nr;
+            this.Nazwa = nazwa;
+            this.Rodzaj = rodzaj;
+            this.Data = data;
+        }
+    }
+}
--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -125,7 +125,7 @@ namespace ManageYourTime {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ManageYourTime;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ManageYourTime;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
